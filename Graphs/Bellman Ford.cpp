de0b8c1ab79d33b43bf91e9a4f062b96e0e44805
pH-1491 <priyeharsh19@gmail.com>
2025-12-30 01:44:22 +0530

    added bellma ford algo

--- Graphs/Bellman Ford.cpp
+++ Graphs/Bellman Ford.cpp
@@ -1,3 +1,47 @@
-//
-// Created by PRIYE HARSH on 30/12/25.
-//
+#include <bits/stdc++.h>
+using namespace std;
+
+
+struct Edge {
+    int u, v, w;
+};
+
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<Edge> edges(m);
+    for (auto &e : edges) cin >> e.u >> e.v, e.w;
+
+    int src;
+    cin >> src;
+
+    const long long INF = 1e18;
+    vector<long long> dist(n, INF);
+    dist[src] = 0;
+
+
+    // Relax edges n-1 times
+    for (int i = 0; i < n - 1; i++) {
+        for (auto &e : edges) {
+            if (dist[e.u] != INF && dist[e.u] + e.w < dist[e.v]) {
+                dist[e.v] = dist[e.u] + e.w;
+            }
+        }
+    }
+
+    // Check negative cycle
+    bool negCycle = false;
+    for (auto &e : edges) {
+        if (dist[e.u] != INF && dist[e.u] + e.w < dist[e.v]) {
+            negCycle = true;
+            break;
+        }
+    }
+
+    if (negCycle) cout << "Negative Weight Cycle Detected\n";
+    else {
+        for (int i = 0; i < n; i++)
+            cout << "Distance to " << i << " = " << dist[i] << "\n";
+    }
+}
